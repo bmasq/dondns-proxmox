c774740709578e124a0aa0603fc653eac7c23dca
Bernat Mas <bernat.masquetglas@gmail.com>
2023-01-28 13:58:43 +0100

    Succesfully update DNS records

--- updateIP.py
+++ updateIP.py
@@ -15,6 +15,7 @@ except PermissionError:
 except Exception as e:
     print(f"ERROR: An unexpected error occurred while opening and reading the file {filename}:", e)
 
+# creates Containers from "pct config" output
 containers = list()
 for id, hostname in ctdict.items():
     try:
@@ -25,7 +26,14 @@ for id, hostname in ctdict.items():
         continue
     containers.append(Container(id, hostname, call.stdout))
 
+# updates DNS record on DonDominio for each container
 for ct in containers:
-    print("id:", ct.id)
-    print("hn:", ct.hostname)
-    print("ip:", ct.ip)
+    command = ["dondns/dondns.sh", "-c", "dondns/dondns.conf",
+                "-h", ct.hostname, "-i", ct.ip]
+    try:
+        call = subprocess.run(command, capture_output=True, text=True, check=True)
+        output = call.stdout.replace('\n', ' ')
+        print(f"{ct.hostname} ({ct.id}):  {output}")
+    except subprocess.CalledProcessError as e:
+        print(f"ERROR for {ct.hostname} ({ct.id}): code {e.returncode}: {e.stderr}")
+        continue
